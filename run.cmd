0679ad7a6f9a8342fe16201fe8ccaa303e42be6d
Quang Le <me@quangld.com>
2016-09-23 20:41:19 +0700

    add .gitignore to run

--- run.cmd
+++ run.cmd
@@ -1,3 +1,6 @@
-rem this requires PM2 to run http://pm2.keymetrics.io/
-pm2 start app.js --name SmartHome --watch --ignore-watch ".git node_modules test public config *.json"
-start pm2 logs
+#!/bin/bash
+#this requires PM2 to run http://pm2.keymetrics.io/
+
+pm2 restart app.js --name SmartHome --watch --ignore-watch ".git .gitignore node_modules test public *.json"
+pm2 logs
+
