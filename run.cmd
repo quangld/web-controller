bb889ea3b244aff40bb9d8e2967a74a5b6fa25ed
Quang Le <me@quangld.com>
2016-09-23 21:07:28 +0700

    update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,6 +1,6 @@
 #!/bin/bash
 #this requires PM2 to run http://pm2.keymetrics.io/
 
-pm2 restart app.js --name SmartHome --watch --ignore-watch ".git .gitignore node_modules test public *.json"
+pm2 start app.js --name SmartHome --watch --ignore-watch ".git .gitignore node_modules test public *.json"
 pm2 logs
 
